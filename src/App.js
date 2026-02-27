ba982cab606363cd461009d6d043be5b98f68b61
Nikita I <vt231_ini@student.ztu.edu.ua>
2026-02-27 13:40:20 +0200

    added gdpr cookie banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 ﻿import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CookieConsent from "react-cookie-consent"; //cookies added (2/26/2026) 
 
 import StartPage from "./pages/StartPage";
 import GamePage from "./pages/GamePage";
@@ -17,6 +18,25 @@ export default function App(){
           <Route path="/result/:userId" element={<ResultPage />} />
           <Route path="/settings" element={<SettingsPage />} />
         </Routes>
+		
+		
+		{/* cookies */}
+        <CookieConsent
+          location="bottom"
+          buttonText="Прийняти"
+          declineButtonText="Тільки необхідні"
+          enableDeclineButton
+          cookieName="emojiGameCookieConsent"
+          style={{background: "#2B373B", fontSize: "14px", alignItems:"center"}}
+          buttonStyle={{backgroundColor:"#10b981",color: "#fff",fontSize: "13px",borderRadius: "5px",padding: "8px 15px",fontWeight: "bold"}} 
+          declineButtonStyle={{backgroundColor:"#ef4444",color: "#fff",fontSize: "13px",borderRadius:"5px",padding: "8px 15px"}} 
+          expires={150}
+        >
+          Ми використовуємо файли cookie для збереження вашого прогресу{" "}
+          <a href="/privacy-policy.md"style={{ color: "#3b82f6", textDecoration: "underline", marginLeft: "10px" }}> 
+            Політика конфіденційності</a>
+        </CookieConsent>
+
       </div>
     </Router>
   );
